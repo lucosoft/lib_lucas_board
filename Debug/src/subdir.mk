2c3d88b964e82016d22b6d583d263e9e457571b2
MGUERRERO\Lucas Guerrero <manuel.guerrero@intive-fdv.com>
2017-10-06 18:15:45 -0300

    first commit

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/LED.c 
+
+OBJS += \
+./src/LED.o 
+
+C_DEPS += \
+./src/LED.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -DCORE_M3 -D__USE_LPCOPEN -D__LPC17XX__ -I"C:\Users\Manuel Guerrero\TD2\WorkspaceTest3\lib_lucas_board\inc" -I"C:\Users\Manuel Guerrero\TD2\WorkspaceTest3\lpc_board_nxp_lpcxpresso_1769\inc" -I"C:\Users\Manuel Guerrero\TD2\WorkspaceTest3\lpc_chip_175x_6x\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m3 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
